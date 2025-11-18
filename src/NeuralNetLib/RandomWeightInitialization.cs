ec6cb8eb7da56188cd7914f7f7de5ffc00b522b2
AilurusApps <brian@ailurus.com.au>
2025-11-19 01:28:39 +1100

    Add xavier weight initialization

---
+++ src/NeuralNetLib/RandomWeightInitialization.cs
@@ -0,0 +1,11 @@
+﻿namespace AilurusApps.NeuralNetLib
+{
+    public class RandomWeightInitialization(Random random) : IWeightInitializationStrategy
+    {
+        public static readonly RandomWeightInitialization Instance = new(Random.Shared);
+        public double GetInitialWeight(int inputCount, int outputCount)
+        {
+            return 0.49 + (random.NextDouble() * 0.02);
+        }
+    }
+}
